dff556e009eb2d3bf9641e94db38402ab714447f
vividlybubblyblue <bluesmiler0331@gmail.com>
2025-07-28 16:50:41 +0900

    feat: prevent sound output when muted

--- script.js
+++ script.js
@@ -127,7 +127,11 @@ function playSound(soundElement) {
 	} else if (soundType.includes('sfx') && !document.getElementById("nosfx").checked) {
 		return;
 	}
-	soundElement.volume = document.getElementById("volume").value / 100;
+	const volume = document.getElementById("volume").value
+	if (volume == 0) {
+		return;
+	}
+	soundElement.volume = volume / 100;
 	soundElement.play();
 }
 
